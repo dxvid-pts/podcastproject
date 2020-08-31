3a1e7e6864aa6a5ba03ca6298f827cd2b72b20a5
peterscodee <8vsxdxd@gmail.com>
2020-08-31 14:55:12 +0200

    Support for importing / exporting data

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,13 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:podcast_player/widgets/settings_section_widget.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+
+import '../analyzer.dart';
 
 const Color greyAccent = const Color(0x09000000);
 const double paddingHorizontal = 26;
@@ -43,6 +51,79 @@ class SettingsScreen extends StatelessWidget {
                   ],
                   initialIndex: 2,
                 ),
+                Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: paddingHorizontal, vertical: 22),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Export & Import',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 4, bottom: 16),
+                        child: Text(
+                          'Backup app data such as RSS feeds',
+                          style: Theme.of(context).textTheme.subtitle1,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          OutlinedButton(
+                            onPressed: () async {
+                              if (!await Permission.storage.request().isGranted)
+                                return;
+
+                              final String dirPath =
+                                  await FilePicker.getDirectoryPath();
+
+                              final String filePath =
+                                  dirPath + Platform.pathSeparator + 'data.pd';
+
+                              print(dirPath);
+
+                              final ProgressDialog pr = ProgressDialog(context);
+                              pr.style(
+                                  progressWidget: CircularProgressIndicator(
+                                    valueColor: AlwaysStoppedAnimation<Color>(
+                                        Colors.blue),
+                                  ),
+                                  message: 'Exporting data to $filePath');
+
+                              await pr.show();
+
+                              File file = File(filePath);
+                              file.createSync();
+                              file.writeAsStringSync(export());
+
+                              await pr.hide();
+                            },
+                            child: Text('Export app data'),
+                          ),
+                          SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: () async {
+                              if (!await Permission.storage.request().isGranted)
+                                return;
+
+                              File file = await FilePicker.getFile(
+                                allowedExtensions: ['pd'],
+                                type: FileType.custom,
+                              );
+                              if (!file.path.endsWith('.pd')) return;
+
+                              print('import ${file.path}');
+                              import(file.readAsStringSync());
+                            },
+                            child: Text('Import app data'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
             Padding(
@@ -72,5 +153,33 @@ class SettingsScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+String export() {
+  Map<String, dynamic> data = Map();
+  for (String key in prefs.getKeys())
+    data.putIfAbsent(key, () => prefs.get(key));
+
+  return jsonEncode(data);
+}
+
+void import(String json) {
+  Map<String, dynamic> data = jsonDecode(json);
+  for (final String key in data.keys) {
+    final value = data[key];
+    switch (value.runtimeType) {
+      case String:
+        prefs.setString(key, value);
+        break;
+      case int:
+        prefs.setInt(key, value);
+        break;
+        //TODO: Not working yet
+      case Iterable:
+        prefs.setStringList(key, value);
+        break;
+    }
+    print(value.runtimeType.toString());
   }
 }
