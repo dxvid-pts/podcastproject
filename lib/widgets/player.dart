c19951de24a255cbdfda040b5b1609724231b628
David <8vsxdxd@gmail.com>
2020-11-26 14:27:52 +0100

    Update dependencies

--- lib/widgets/player.dart
+++ lib/widgets/player.dart
@@ -258,7 +258,7 @@ class _AudioControllerWidgetState extends State<AudioControllerWidget> {
       minHeight: playerMinHeight,
       maxHeight: playerMaxHeight,
       valueNotifier: playerExpandProgress,
-      onDismiss: () {
+      onDismissed: () {
         currentlyPlaying.value = null;
         AudioService.stop();
       },
